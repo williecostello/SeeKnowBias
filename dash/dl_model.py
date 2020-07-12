c3a720a9b5e692fb2d79065e4d9edbfa2558d581
Nicholas Broad <nbroad94@gmail.com>
2020-07-12 14:31:23 -0400

    forgot colon

--- dash/dl_model.py
+++ dash/dl_model.py
@@ -6,6 +6,6 @@ import logging
 
 
 def download():
-    if not (Path.cwd()/"model.pkl").exists()
+    if not (Path.cwd()/"model.pkl").exists():
         url = 'https://docs.google.com/uc?export=download&id=1x__YR6PO6_cW-vy3vb0cdjWRYqjBqPW0'
         urllib.request.urlretrieve(url, "./model.pkl")
